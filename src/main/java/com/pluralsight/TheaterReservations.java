b38451764532b0c6cc7b118853decfcbfa8d41b1
Taihvonne Rattanavong <tdrattanavong@gmail.com>
2026-04-14 17:24:03 -0700

    initial commit

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class TheaterReservations {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name");
+        String fullName = scanner.nextLine();
+
+        System.out.println("What date will you be attending (YYYY-MM-DD)?");
+        String input = scanner.nextLine();
+        LocalDate reservation = LocalDate.parse(input);
+
+        System.out.println("How many tickets will you need");
+        String tickets = scanner.nextLine();
+
+
+        System.out.println(reservation);
+        System.out.println(input);
+    }
 }
